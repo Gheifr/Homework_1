6b8fb15504922ae6dac35a6b6ee1356aee5c114a
Gheifr <v.prushak@gmail.com>
2019-03-25 23:21:33 +0200

    save 1

--- Homework_1.cs
+++ Homework_1.cs
@@ -10,6 +10,21 @@ namespace Homework_1
     {
         static void Main(string[] args)
         {
+            string strMsg = "Hi dude!" + (char)10;
+
+            strMsg += "Please select the task from list below:" + (char)10;
+            strMsg += "1. Task No 1" + (char)10;
+            strMsg += "2. Task No 2" + (char)10;
+            strMsg += "3. Task No 3" + (char)10;
+            strMsg += "4. Task No 4" + (char)10;
+            strMsg += "5. Task No 5" + (char)10;
+            strMsg += "6. Task No 6" + (char)10;
+            strMsg += "7. Task No 7" + (char)10;
+            strMsg += "8. Task No 8" + (char)10;
+
+            Console.WriteLine(strMsg);
+            Console.ReadKey();
+
         }
     }
 }
