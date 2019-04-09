0d4d679ca9a104edd604425187a21bcb41d17ee1
Gheifr <Gheifr@users.noreply.github.com>
2019-04-09 20:30:27 +0300

    2

--- Lesson 5/People.cs
+++ Lesson 5/People.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace Lesson_5
 {
-    class People
+    public class People
     {
+        public int Age { get; private set; }
+        public string Name { get; private set; }
+
+        public People(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public virtual void GoToTheZoo()
+        {
+            Console.WriteLine("I'm going to the zoo!")
+        }
     }
 }
