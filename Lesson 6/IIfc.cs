e4989fa9b9edd1251864da5a21ce168268cbb14e
Gheifr <Gheifr@users.noreply.github.com>
2019-04-11 19:55:11 +0300

    2

---
+++ Lesson 6/IIfc.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_6
+{
+    public interface IIfc
+    {
+        void Use();
+    }
+}
--- Lesson 6/Program.cs
+++ Lesson 6/Program.cs
@@ -6,7 +6,10 @@ namespace Lesson_6
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+
+
         }
     }
 }
