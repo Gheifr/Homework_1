b09a220c17cf3e3899a0b689364a987d379fe0c5
Gheifr <Gheifr@users.noreply.github.com>
2019-04-16 21:01:20 +0300

    2

---
+++ Calculator/ViewModel/PropertyChangedViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.ViewModel
+{
+    class PropertyChangedViewModel : INotifyPropertyChanged
+    {
+        protected void NotifyPropertyChange([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
