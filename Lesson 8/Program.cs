be68d5145cb368ae0974a5c9167c70a6dfa2bcbc
Gheifr <Gheifr@users.noreply.github.com>
2019-04-18 20:09:15 +0300

    2

---
+++ Lesson 8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//exceptions//
+namespace Lesson_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                File.Open(@"C:\file.txt", FileMode.Open);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                //code that will be executed almost always ;) 
+                //fail-fast type of application - app will crash at the time of exception at once - catch and finally will not work-out at all
+            }
+        }
+    }
+}
