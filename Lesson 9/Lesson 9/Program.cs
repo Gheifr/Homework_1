b9ab908c371bb5b3acf8f02f1fc88ec431632c50
Gheifr <Gheifr@users.noreply.github.com>
2019-04-23 21:36:10 +0300

    4

--- Lesson 9/Lesson 9/Program.cs
+++ Lesson 9/Lesson 9/Program.cs
@@ -11,13 +11,41 @@ namespace Lesson_9
         static void Main(string[] args)
         {
 
-            for (int i = 700; i >=200; i-=13)
-            {
-                Console.WriteLine(i);
-            }
+            //Console.Write(CheckPalindrome(Console.ReadLine()));
+
+            Console.WriteLine("Enter A");
+            int A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter B");
+            int B = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"A = {A}\nB = {B}");
+           
+            A = A + B;
+            B = A - B;
+            A = A - B;
+
+            Console.WriteLine($"A: {A}; B: {B}");
             Console.ReadKey();
+        
+        }
+
+        public static bool CheckPalindrome(string text)
+        {
+            if (text == null)
+                return false;
+
+            string test = text.ToLower();
+
+            test = test.Trim();
+
+                for (int i=0;i< test.Length;i++)
+                {
+                    if (test[i] != test[test.Length - i - 1])
+                        return false; 
+                }
+                return true;
 
         }
-        
+
     }
 }
