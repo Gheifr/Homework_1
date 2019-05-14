155528b3c11437d882fae5723f3f0aae3d0e6da0
Gheifr <Gheifr@users.noreply.github.com>
2019-05-14 19:53:40 +0300

    start

---
+++ C3L1/MyList.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3L1
+{
+
+    public class MyList <T>
+    {
+        public MyList()
+        {
+          
+        }
+
+        private T[] items = new T[0];
+
+
+        public void addItem(T item)
+        {
+            Array.Resize(ref items, items.Count() + 1);
+            items[items.Count() - 1] = item;
+        }
+
+        public void removeItem(T item)
+        {
+            items[Array.IndexOf(items, item)] = items[items.Count() - 1];
+            Array.Resize(ref items, items.Count() - 1);
+        }
+
+        public IEnumerable<T> getItems()
+        {
+            return items;
+        }
+    }
+
+
+}
---
+++ C3L1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C3L1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyList myList = new MyList();
+
+
+        }
+    }
+}
