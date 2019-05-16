5d2ba5e6aab531f7975f52b31fc4cc6eb9ad67e4
Gheifr <Gheifr@users.noreply.github.com>
2019-05-16 19:12:12 +0300

    1

--- C3L2/Program.cs
+++ C3L2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace C3L2
     {
         static void Main(string[] args)
         {
+
+
+
         }
     }
 }
